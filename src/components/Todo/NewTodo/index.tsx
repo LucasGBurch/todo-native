495ecafc9c4a907f6435f849f077c884881f78ec
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-06-24 17:09:52 -0300

    NewTodo quase estilizado, falta o + redondo

--- src/components/Todo/NewTodo/index.tsx
+++ src/components/Todo/NewTodo/index.tsx
@@ -1,5 +1,21 @@
-import { View } from "react-native";
+import { TextInput, TouchableOpacity, View, Text } from 'react-native';
+import { styles } from './styles';
 
 export function NewTodo() {
-  return <View></View>
+  return (
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        placeholder='Adicione uma nova tarefa'
+        placeholderTextColor='#808080'
+        keyboardType='default'
+      />
+
+      <TouchableOpacity style={styles.button}>
+        <View style={styles.buttonCircle}>
+          <Text style={styles.buttonText}>+</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
 }
---
+++ src/components/Todo/NewTodo/styles.ts
@@ -0,0 +1,49 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  form: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginBottom: 32,
+    top: '-7%',
+    width: 327,
+  },
+
+  input: {
+    alignItems: 'center',
+    backgroundColor: '#262626',
+    borderRadius: 6,
+    color: '#F2F2F2',
+    flex: 1,
+    height: 54,
+    fontSize: 16,
+    marginRight: 12,
+    padding: 16,
+    width: 327,
+  },
+
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#1E6F9F',
+    borderRadius: 6,
+    height: 52,
+    width: 52,
+  },
+
+  buttonCircle: {
+    backgroundColor: '#1E6F9F',
+    borderColor: '#F2F2F2',
+    borderStyle: 'solid',
+    borderRadius: 1000,
+    height: 18,
+    width: 18,
+  },
+
+  buttonText: {
+    color: '#F2F2F2',
+    fontSize: 18,
+  },
+});
